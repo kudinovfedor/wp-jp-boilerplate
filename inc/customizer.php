5a1f4cd2fdcc15d5548957152ef31aa21cc522e0
Kudinov Fedor <brothersrabbits@mail.ru>
2018-05-31 17:50:56 +0300

    Add to customizer Google Map settings for JS API

--- inc/customizer.php
+++ inc/customizer.php
@@ -520,6 +520,347 @@ function jp_customize_register($wp_customize)
         'type'     => 'tel',
     ));
 
+    // Panel Google Map
+    $wp_customize->add_panel('google_map', array(
+        'title'       => 'Google Map',
+        'description' => 'Customizer for Google Map',
+        'priority'    => 202,
+    ));
+
+    // Section Project Setup
+    $wp_customize->add_section('google_map_project_setup', array(
+        'title' => 'Project setup',
+        'panel' => 'google_map',
+    ));
+
+    $wp_customize->add_setting('google_map_project_setup_api_key', array('default' => '',));
+    $wp_customize->add_setting('google_map_project_setup_map_selector', array('default' => 'google-map',));
+    $wp_customize->add_setting('google_map_project_setup_height', array('default' => 400,));
+    $wp_customize->add_setting('google_map_project_setup_width', array('default' => 600,));
+    $wp_customize->add_setting('google_map_project_setup_latitude', array('default' => '',));
+    $wp_customize->add_setting('google_map_project_setup_longitude', array('default' => '',));
+    $wp_customize->add_setting('google_map_project_setup_zoom_level', array('default' => 3,));
+
+    $wp_customize->add_control('google_map_project_setup_api_key', array(
+        'label'       => 'Google Maps API Key',
+        'description' => 'All Google Maps JavaScript API applications require authentication.',
+        'section'     => 'google_map_project_setup',
+        'settings'    => 'google_map_project_setup_api_key',
+        'type'        => 'text',
+    ));
+
+    $wp_customize->add_control('google_map_project_setup_map_selector', array(
+        'label'    => 'Map selector (for css)',
+        'section'  => 'google_map_project_setup',
+        'settings' => 'google_map_project_setup_map_selector',
+        'type'     => 'text',
+    ));
+
+    $wp_customize->add_control('google_map_project_setup_height', array(
+        'label'       => 'Height',
+        'section'     => 'google_map_project_setup',
+        'settings'    => 'google_map_project_setup_height',
+        'type'        => 'number',
+        'input_attrs' => array(
+            'min'  => 0,
+            'step' => 1,
+        ),
+    ));
+
+    $wp_customize->add_control('google_map_project_setup_width', array(
+        'label'       => 'Width',
+        'section'     => 'google_map_project_setup',
+        'settings'    => 'google_map_project_setup_width',
+        'type'        => 'number',
+        'input_attrs' => array(
+            'min'  => 0,
+            'step' => 1,
+        ),
+    ));
+
+    $wp_customize->add_control('google_map_project_setup_latitude', array(
+        'label'    => 'Latitide',
+        'section'  => 'google_map_project_setup',
+        'settings' => 'google_map_project_setup_latitude',
+        'type'     => 'text',
+    ));
+
+    $wp_customize->add_control('google_map_project_setup_longitude', array(
+        'label'    => 'Longitude',
+        'section'  => 'google_map_project_setup',
+        'settings' => 'google_map_project_setup_longitude',
+        'type'     => 'text',
+    ));
+
+    $wp_customize->add_control('google_map_project_setup_zoom_level', array(
+        'label'       => 'Zoom level',
+        'section'     => 'google_map_project_setup',
+        'settings'    => 'google_map_project_setup_zoom_level',
+        'type'        => 'number',
+        'input_attrs' => array(
+            'min'  => 0,
+            'max'  => 20,
+            'step' => 1,
+        ),
+    ));
+
+    // Section Controls
+    $wp_customize->add_section('google_map_controls', array(
+        'title' => 'Controls',
+        'panel' => 'google_map',
+    ));
+
+    $wp_customize->add_setting('google_map_controls_map_type', array('default' => 'horizontal_bar',));
+    $wp_customize->add_setting('google_map_controls_zoom', array('default' => 1,));
+    $wp_customize->add_setting('google_map_controls_gesture_handling', array('default' => 'auto',));
+    $wp_customize->add_setting('google_map_controls_full_screen', array('default' => 0,));
+    $wp_customize->add_setting('google_map_controls_street_view', array('default' => 1,));
+    $wp_customize->add_setting('google_map_controls_scale', array('default' => 1,));
+    $wp_customize->add_setting('google_map_controls_clickable_poi', array('default' => 0,));
+    $wp_customize->add_setting('google_map_controls_draggable', array('default' => 1,));
+    $wp_customize->add_setting('google_map_controls_double_click_to_zoom', array('default' => 1,));
+    $wp_customize->add_setting('google_map_controls_mouse_wheel_to_zoom', array('default' => 1,));
+
+    $wp_customize->add_control('google_map_controls_map_type', array(
+        'label'    => 'Map type',
+        'section'  => 'google_map_controls',
+        'settings' => 'google_map_controls_map_type',
+        'type'     => 'select',
+        'choices'  => array(
+            'false'          => 'Hide',
+            'horizontal_bar' => 'Horizontal Bar',
+            'dropdown_menu'  => 'Dropdown Menu',
+        ),
+    ));
+
+    $wp_customize->add_control('google_map_controls_zoom', array(
+        'label'    => 'Zoom',
+        'section'  => 'google_map_controls',
+        'settings' => 'google_map_controls_zoom',
+        'type'     => 'select',
+        'choices'  => array(
+            0 => 'Hide',
+            1 => 'Show',
+        ),
+    ));
+
+    $wp_customize->add_control('google_map_controls_gesture_handling', array(
+        'label'    => 'Gesture handling',
+        'section'  => 'google_map_controls',
+        'settings' => 'google_map_controls_gesture_handling',
+        'type'     => 'select',
+        'choices'  => array(
+            'none'        => 'None',
+            'auto'        => 'Auto',
+            'greedy'      => 'Greedy',
+            'cooperative' => 'Cooperative',
+        ),
+    ));
+
+    $wp_customize->add_control('google_map_controls_full_screen', array(
+        'label'    => 'Full screen',
+        'section'  => 'google_map_controls',
+        'settings' => 'google_map_controls_full_screen',
+        'type'     => 'select',
+        'choices'  => array(
+            0 => 'Hide',
+            1 => 'Show',
+        ),
+    ));
+
+    $wp_customize->add_control('google_map_controls_street_view', array(
+        'label'    => 'Street view',
+        'section'  => 'google_map_controls',
+        'settings' => 'google_map_controls_street_view',
+        'type'     => 'select',
+        'choices'  => array(
+            0 => 'Hide',
+            1 => 'Show',
+        ),
+    ));
+
+    $wp_customize->add_control('google_map_controls_scale', array(
+        'label'    => 'Scale',
+        'section'  => 'google_map_controls',
+        'settings' => 'google_map_controls_scale',
+        'type'     => 'select',
+        'choices'  => array(
+            0 => 'Hide',
+            1 => 'Show',
+        ),
+    ));
+
+    $wp_customize->add_control('google_map_controls_clickable_poi', array(
+        'label'    => 'Clickable POI',
+        'section'  => 'google_map_controls',
+        'settings' => 'google_map_controls_clickable_poi',
+        'type'     => 'select',
+        'choices'  => array(
+            0 => 'Disable',
+            1 => 'Enable',
+        ),
+    ));
+
+    $wp_customize->add_control('google_map_controls_draggable', array(
+        'label'    => 'Draggable',
+        'section'  => 'google_map_controls',
+        'settings' => 'google_map_controls_draggable',
+        'type'     => 'select',
+        'choices'  => array(
+            0 => 'Disable',
+            1 => 'Enable',
+        ),
+    ));
+
+    $wp_customize->add_control('google_map_controls_double_click_to_zoom', array(
+        'label'    => 'Double click to zoom',
+        'section'  => 'google_map_controls',
+        'settings' => 'google_map_controls_double_click_to_zoom',
+        'type'     => 'select',
+        'choices'  => array(
+            0 => 'Disable',
+            1 => 'Enable',
+        ),
+    ));
+
+    $wp_customize->add_control('google_map_controls_mouse_wheel_to_zoom', array(
+        'label'    => 'Mouse wheel to zoom',
+        'section'  => 'google_map_controls',
+        'settings' => 'google_map_controls_mouse_wheel_to_zoom',
+        'type'     => 'select',
+        'choices'  => array(
+            0 => 'Disable',
+            1 => 'Enable',
+        ),
+    ));
+
+    // Section Positions
+    $wp_customize->add_section('google_map_positions', array(
+        'title' => 'Positions',
+        'panel' => 'google_map',
+    ));
+
+    $wp_customize->add_setting('google_map_positions_map_type', array('default' => 10,));
+    $wp_customize->add_setting('google_map_positions_zoom', array('default' => 6,));
+    $wp_customize->add_setting('google_map_positions_street_view', array('default' => 6,));
+    $wp_customize->add_setting('google_map_positions_full_screen', array('default' => 11,));
+
+    $wp_customize->add_control('google_map_positions_map_type', array(
+        'label'    => 'Map type',
+        'section'  => 'google_map_positions',
+        'settings' => 'google_map_positions_map_type',
+        'type'     => 'select',
+        'choices'  => array(
+            0  => 'Bottom Center',
+            1  => 'Bottom Left',
+            2  => 'Bottom Right',
+            3  => 'Left Bottom',
+            4  => 'Left Center',
+            5  => 'Left Top',
+            6  => 'Right Bottom',
+            7  => 'Right Center',
+            8  => 'Right Top',
+            9  => 'Top Center',
+            10 => 'Top Left',
+            11 => 'Top Right',
+        ),
+    ));
+
+    $wp_customize->add_control('google_map_positions_zoom', array(
+        'label'    => 'Zoom',
+        'section'  => 'google_map_positions',
+        'settings' => 'google_map_positions_zoom',
+        'type'     => 'select',
+        'choices'  => array(
+            0  => 'Bottom Center',
+            1  => 'Bottom Left',
+            2  => 'Bottom Right',
+            3  => 'Left Bottom',
+            4  => 'Left Center',
+            5  => 'Left Top',
+            6  => 'Right Bottom',
+            7  => 'Right Center',
+            8  => 'Right Top',
+            9  => 'Top Center',
+            10 => 'Top Left',
+            11 => 'Top Right',
+        ),
+    ));
+
+    $wp_customize->add_control('google_map_positions_street_view', array(
+        'label'    => 'Street view',
+        'section'  => 'google_map_positions',
+        'settings' => 'google_map_positions_street_view',
+        'type'     => 'select',
+        'choices'  => array(
+            0  => 'Bottom Center',
+            1  => 'Bottom Left',
+            2  => 'Bottom Right',
+            3  => 'Left Bottom',
+            4  => 'Left Center',
+            5  => 'Left Top',
+            6  => 'Right Bottom',
+            7  => 'Right Center',
+            8  => 'Right Top',
+            9  => 'Top Center',
+            10 => 'Top Left',
+            11 => 'Top Right',
+        ),
+    ));
+
+    $wp_customize->add_control('google_map_positions_full_screen', array(
+        'label'    => 'Full screen',
+        'section'  => 'google_map_positions',
+        'settings' => 'google_map_positions_full_screen',
+        'type'     => 'select',
+        'choices'  => array(
+            0  => 'Bottom Center',
+            1  => 'Bottom Left',
+            2  => 'Bottom Right',
+            3  => 'Left Bottom',
+            4  => 'Left Center',
+            5  => 'Left Top',
+            6  => 'Right Bottom',
+            7  => 'Right Center',
+            8  => 'Right Top',
+            9  => 'Top Center',
+            10 => 'Top Left',
+            11 => 'Top Right',
+        ),
+    ));
+
+    // Section Positions
+    $wp_customize->add_section('google_map_themes', array(
+        'title' => 'Themes',
+        'panel' => 'google_map',
+    ));
+
+    $wp_customize->add_setting('google_map_themes_type', array('default' => 'roadmap',));
+    $wp_customize->add_setting('google_map_themes_styles', array('default' => 0,));
+
+    $wp_customize->add_control('google_map_themes_type', array(
+        'label'    => 'Google maps theme',
+        'section'  => 'google_map_themes',
+        'settings' => 'google_map_themes_type',
+        'type'     => 'select',
+        'choices'  => array(
+            'roadmap'   => 'Road Map',
+            'satellite' => 'Satellite',
+            'hybrid'    => 'Hybrid',
+            'terrain'   => 'Terrain',
+        ),
+    ));
+
+    $wp_customize->add_control('google_map_themes_styles', array(
+        'label'    => 'Shazzy maps theme',
+        'section'  => 'google_map_themes',
+        'settings' => 'google_map_themes_styles',
+        'type'     => 'select',
+        'choices'  => array(
+            0 => 'None',
+        ),
+    ));
+
 }
 
 add_action('customize_register', 'jp_customize_register');
